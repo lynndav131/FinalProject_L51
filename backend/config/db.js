223976eabf6dce29bd6118c8a3adf206cab41877
Lynnox Davis <ldavi131@charlotte.edu>
2025-12-07 00:41:35 -0500

    update to remove wrapper for db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
@@ -11,5 +11,4 @@ const pool = mysql.createPool({
   queueLimit: 0
 });
 
-// Export promise-based pool
-module.exports = pool.promise();
+module.exports = pool;
