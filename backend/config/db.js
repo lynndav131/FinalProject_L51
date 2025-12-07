1872427fec75cdcadc61b8fe9729d54b90563d30
Lynnox Davis <ldavi131@charlotte.edu>
2025-12-07 18:07:10 -0500

    connect to DB using cert

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,16 @@
+const fs = require('fs');
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '',
-  database: process.env.DB_NAME || 'projectdb',
-  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  ssl: {
+    ca: fs.readFileSync(__dirname + '/../certs/ca-certificate.crt'),
+    rejectUnauthorized: true
+  }
 });
 
 module.exports = pool;
